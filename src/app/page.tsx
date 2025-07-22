a22f5ff478290999b742bf0cb205167fc7940564
bar47ney <bar47ney@gmail.com>
2025-07-22 15:16:59 +0300

    first-commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,116 @@
-import Image from "next/image";
+"use client";
+
+import { FFmpeg } from "@ffmpeg/ffmpeg";
+import { toBlobURL } from "@ffmpeg/util";
+import { VideoIcon } from "lucide-react";
+import { use, useEffect, useRef, useState } from "react";
+
+interface VideoPlayerState {
+  isPlaying: boolean;
+  trimRange: [number, number];
+  video: string | null;
+  currentTime: number;
+}
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const [status, setStatus] = useState("loading");
+  const [error, setError] = useState<string | null>(null);
+  const [video, setVideo] = useState<File | null>(null);
+  const [videoState, setVideoState] = useState<VideoPlayerState>({
+    isPlaying: false,
+    trimRange: [0, 0],
+    video: null,
+    currentTime: 0,
+  });
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const ffmpegRef = useRef<FFmpeg | null>(null);
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+
+  useEffect(() => {
+    loadFFmpeg();
+  }, []);
+
+  const loadFFmpeg = async () => {
+    try {
+      setStatus("loading ffmpeg...");
+      setError(null);
+
+      const baseURL = "https://unpkg.com/@ffmpeg/core@0.12.15/dist/umd";
+      const ffmpeg = new FFmpeg();
+      ffmpegRef.current = ffmpeg;
+
+      ffmpeg.on("log", ({ message }) => {
+        console.log(message);
+      });
+
+      await ffmpeg.load({
+        coreURL: await toBlobURL(
+          `${baseURL}/ffmpeg-core.js`,
+          "text/javascript"
+        ),
+        wasmURL: await toBlobURL(
+          `${baseURL}/ffmpeg-core.wasm`,
+          "application/wasm"
+        ),
+      });
+
+      setStatus("idle");
+    } catch (error) {
+      console.log(error);
+      setError("Failed to load FFmpeg");
+    }
+  };
+
+  const handleFileChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (!file) setError("No selected file");
+    if (file) {
+      const url = URL.createObjectURL(file);
+      setVideo(file);
+      setVideoState({ ...videoState, video: url });
+    }
+  };
+
+  return (
+    <div className="h-screen w-full max-w-4xl mx-auto">
+      <div className="grid grid-rows-[1fr_auto] h-full">
+        <div className="bg-gray-900 w-full h-full relative overflow-hidden">
+          {videoState.video ? (
+            <div className="absolute inset-0">
+              <video
+                ref={videoRef}
+                src={videoState.video}
+                controls
+                className="w-full h-full object-contain inset-0 flex items-center justify-center"
+              ></video>
+            </div>
+          ) : (
+            <div className="absolute inset-0 flex flex-col items-center p-8">
+              <div className="flex flex-col items-center justify-center border border-dashed border-white/25 px-6 py-10">
+                <div>
+                  <VideoIcon className="mx-auto h-12 w-12 text-gray-400" />
+                </div>
+                <div className="flex items-center justify-center text-sm leading-6 text-gray-400">
+                  <label className="relative cursor-pointer rounded-md bg-gray-900 font-semibold text-white">
+                    <span>Upload video </span>
+                    <input
+                      type="file"
+                      accept="video/*"
+                      id="file-upload"
+                      name="file-upload"
+                      onChange={handleFileChange}
+                    />
+                  </label>
+                </div>
+                <p className="text-xs leading-5 text-gray-400 mt-2">
+                  MP4, MOV and AVI upto 100MB
+                </p>
+              </div>
+            </div>
+          )}
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className="bg-gray-800 flex flex-col space-y-4">x</div>
+      </div>
     </div>
   );
 }
