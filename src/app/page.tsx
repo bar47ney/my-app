26880e0425f2bb2cf2c0a7e0a4aaad2163a79bbe
bar47ney <bar47ney@gmail.com>
2025-07-22 17:30:29 +0300

    add timeline

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,6 +12,63 @@ interface VideoPlayerState {
   currentTime: number;
 }
 
+type RangeSliderProps = {
+  min: number;
+  max: number;
+  step: number;
+  value: [number, number];
+  onChange: (range: [number, number]) => void;
+};
+
+const RangeSlider = ({ min, max, step, value, onChange }: RangeSliderProps) => {
+  const [isDragging, setIsDraggig] = useState<number | null>(null);
+  const rangeRef = useRef<HTMLDivElement | null>(null);
+
+  const handleMouseDown = (
+    e: React.MouseEvent<HTMLDivElement>,
+    handle: number
+  ) => {
+    e.preventDefault();
+    setIsDraggig(handle);
+  };
+
+  useEffect(() => {
+    const handleMouseMove = (e: MouseEvent) => {
+      if (!isDragging || !rangeRef.current) return;
+
+      const rect = rangeRef.current.getBoundingClientRect();
+      const x = e.clientX - rect.left;
+      const percent = x / rect.width;
+      const newValue = min + percent * (max - min);
+
+      const newValueRounded = Math.round(newValue / step) * step;
+
+      if (isDragging === 0) {
+        onChange([newValueRounded, value[1]]);
+      } else {
+        onChange([value[0], newValueRounded]);
+      }
+    };
+  }, [isDragging, max, min, onChange, step, value]);
+
+  return (
+    <div
+      ref={rangeRef}
+      className="relative h-12 bg-gray-700 rounded-lg cursor-pointer"
+    >
+      <div className="absolute inset-0 h-full bg-red-700 rounded-lg"></div>
+      <div
+        className="absolute left-0 bg-white h-12 w-4 rounded-sm shadow cursor-grab active:cursor-grabbing"
+        onMouseDown={(e) => handleMouseDown(e, 0)}
+      />
+      <div
+        className="absolute right-0 bg-white h-12 w-4 rounded-sm shadow cursor-grab active:cursor-grabbing"
+        onMouseDown={(e) => handleMouseDown(e, 1)}
+      />
+    </div>
+  );
+};
+
 export default function Home() {
   const [status, setStatus] = useState("loading");
   const [error, setError] = useState<string | null>(null);
@@ -27,7 +84,7 @@ export default function Home() {
   const videoRef = useRef<HTMLVideoElement | null>(null);
 
   useEffect(() => {
-    loadFFmpeg();
+    // loadFFmpeg();
   }, []);
 
   const loadFFmpeg = async () => {
@@ -71,6 +128,8 @@ export default function Home() {
     }
   };
 
+  const handleTrim = async () => {};
+
   return (
     <div className="h-screen w-full max-w-4xl mx-auto">
       <div className="grid grid-rows-[1fr_auto] h-full">
@@ -109,7 +168,17 @@ export default function Home() {
             </div>
           )}
         </div>
-        <div className="bg-gray-800 flex flex-col space-y-4">x</div>
+        <div className="bg-gray-800 flex flex-col space-y-4">
+          <div className="relative">
+            <RangeSlider
+              min={0}
+              max={100}
+              step={0.1}
+              value={videoState.trimRange}
+              onChange={handleTrim}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
